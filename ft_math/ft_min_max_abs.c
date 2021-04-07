4a7cdad70b62815658707ef51f206afa5057d588
big-shell <>
2021-04-07 10:51:11 +0200

    int_min max and abs at new norm (no ternary)

--- ft_math/ft_min_max_abs.c
+++ ft_math/ft_min_max_abs.c
@@ -6,21 +6,30 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/07 13:16:56 by mjacq             #+#    #+#             */
-/*   Updated: 2020/12/03 12:34:30 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/04/07 10:50:36 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_int_min(int x, int y)
 {
-	return (x < y ? x : y);
+	if (x < y)
+		return (x);
+	else
+		return (y);
 }
 
 int		ft_int_max(int x, int y)
 {
-	return (x > y ? x : y);
+	if (x > y)
+		return (x);
+	else
+		return (y);
 }
 
 int		ft_int_abs(int x)
 {
-	return (x > 0 ? x : -x);
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
 }
