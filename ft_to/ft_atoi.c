3f8ff7b529f22b9d187a23f4b55b0b8161e8b2ac
big-shell <>
2021-05-07 14:57:00 +0200

    fix atoi

--- ft_to/ft_atoi.c
+++ ft_to/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 15:43:48 by mjacq             #+#    #+#             */
-/*   Updated: 2021/05/06 20:30:28 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/05/07 14:27:17 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,14 @@ static int	atoi_isspace(char c)
 }
 
 /*
-** Convert string to int. Positive overflows return (-1), negative overflows (0)
+** Convert string to int.
+** Positive long overflows (> LONG_MAX) return (-1), negative (< LONG_MIN) (0)
 */
 
 int	ft_atoi(const char *str)
 {
-	long	number;
-	int		sign;
+	unsigned long	number;
+	int				sign;
 
 	while (atoi_isspace(*str))
 		str++;
@@ -44,14 +45,49 @@ int	ft_atoi(const char *str)
 		str++;
 	number = 0;
 	while (*str >= '0' && *str <= '9')
-		number = 10 * number + *str++ - '0';
-	if (sign * number > INT_MAX || sign * number < INT_MIN)
 	{
-		if (sign > 0)
+		number = 10 * number + *str++ - '0';
+		if (sign > 0 && number > LONG_MAX)
 			return (-1);
-		else
+		if (sign < 0 && number > (unsigned long)LONG_MAX + 1)
 			return (0);
 	}
-	else
-		return (sign * (int)number);
+	return (sign * (int)number);
 }
+
+/*
+** #include <stdlib.h>
+** int	main(void) {
+** 	ft_putnbr(atoi("-2147483646"));
+** 	ft_putchar('\n');
+** 	ft_putnbr(ft_atoi("-2147483646"));
+** 	ft_putchar('\n');
+** 	ft_putchar('\n');
+** 	ft_putnbr(atoi("-2147483647"));
+** 	ft_putchar('\n');
+** 	ft_putnbr(ft_atoi("-2147483647"));
+** 	ft_putchar('\n');
+** 	ft_putchar('\n');
+** 	ft_putnbr(atoi("-2147483648"));
+** 	ft_putchar('\n');
+** 	ft_putnbr(ft_atoi("-2147483647"));
+** 	ft_putchar('\n');
+** 	ft_putchar('\n');
+** 	ft_putnbr(atoi("-2147483649"));
+** 	ft_putchar('\n');
+** 	ft_putnbr(ft_atoi("-2147483649"));
+** 	ft_putchar('\n');
+** 	ft_putchar('\n');
+** 	ft_putnbr(atoi("99999999999999999999999999"));
+** 	ft_putchar('\n');
+** 	ft_putnbr(ft_atoi("99999999999999999999999999"));
+** 	ft_putchar('\n');
+** 	ft_putchar('\n');
+** 	ft_putnbr(atoi("-99999999999999999999999999"));
+** 	ft_putchar('\n');
+** 	ft_putnbr(ft_atoi("-99999999999999999999999999"));
+** 	ft_putchar('\n');
+** 	ft_putchar('\n');
+** 	return (0);
+** }
+*/
