98dbda64c69b361e32c8ff11719977a72b677310
big-shell <>
2021-05-14 14:50:21 +0200

    fix strchr with unicode

--- ft_str/ft_strchr.c
+++ ft_str/ft_strchr.c
@@ -6,17 +6,25 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 15:06:11 by mjacq             #+#    #+#             */
-/*   Updated: 2021/04/29 19:55:43 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/05/14 14:33:58 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** Return a pointer to the first matching character (int casted to char)
+**
+** Return NULL if input string is NULL. (system's strchr segfaults)
+*/
+
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s && *s != (char)c)
 		s++;
-	if (*s == c)
+	if (*s == (char)c)
 		return ((char *)s);
 	else
 		return (NULL);
