408fc954208ebc0efecde7f0c476547a0e5b68f8
big-shell <>
2021-05-18 18:19:34 +0200

    strdup && strlen protection

--- ft_str/ft_strlen.c
+++ ft_str/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 17:25:59 by mjacq             #+#    #+#             */
-/*   Updated: 2021/04/29 19:53:52 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/05/18 18:08:20 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 /*
 ** Counts bytes in a string ('\0' not included).
+**
+** Note: protected, return 0 if str is NULL (unlike system's strlen)
 */
 
 size_t	ft_strlen(const char *str)
@@ -21,6 +23,8 @@ size_t	ft_strlen(const char *str)
 	size_t	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 		i++;
 	return (i);
