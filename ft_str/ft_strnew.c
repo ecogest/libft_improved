38f394defcf3686b1d53c6447418334b9522e951
big-shell <>
2021-05-18 19:15:51 +0200

    fix strnew

--- ft_str/ft_strnew.c
+++ ft_str/ft_strnew.c
@@ -6,22 +6,17 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 17:08:27 by mjacq             #+#    #+#             */
-/*   Updated: 2020/07/01 11:33:45 by mjacq            ###   ########.fr       */
+/*   Updated: 2021/05/18 19:15:34 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** Allocates (with malloc(3)) and returns a “fresh” string ending with ’\0’.
-** Each character of the string is initialized at ’\0’.
-** If the allocation fails the function returns NULL.
+** Allocates size + 1 characters and set them all to '\0'
 */
 
 char	*ft_strnew(size_t size)
 {
-	if (!size)
-		return (NULL);
-	else
 		return (ft_calloc(size + 1, 1));
 }
