a9193b3af70b0284bebb2510e4ed48634c8621c8
big-shell <>
2021-05-18 10:05:23 +0200

    strlcpy test

--- tests/ft_str/ft_strlcpy_test.cc
+++ tests/ft_str/ft_strlcpy_test.cc
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy_test.cc                                 :+:      :+:    :+:   */
+/*   ft_strlcpy_test.cc                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/17 19:32:54 by mjacq             #+#    #+#             */
-/*   Updated: 2021/05/18 09:52:57 by mjacq            ###   ########.fr       */
+/*   Created: 2021/05/17 20:25:18 by mjacq             #+#    #+#             */
+/*   Updated: 2021/05/18 10:03:19 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,43 +25,40 @@ extern "C" {
 ** =========================== Parametrized Tests =========================== **
 */
 
-class FtStrncpyParamTest:
+class FtStrlcpyParamTest:
 	public ::testing::TestWithParam<std::tuple<const char *, size_t>>
 {
 public:
 	char		dest_mine[20];
 	char		dest_system[20];
-	char		*ret_mine;
-	char		*ret_system;
-	FtStrncpyParamTest () {
-		// bzero(dest_mine, 20);
-		// bzero(dest_system, 20);
-		// dest is not necessarily NULL terminated, we want be sure when it does
+	size_t		ret_mine;
+	size_t		ret_system;
+	FtStrlcpyParamTest () {
 		memset(dest_mine, 'x', 20);
 		memset(dest_system, 'x', 20);
 		dest_mine[19] = '\0';
 		dest_system[19] = '\0';
-		ret_mine = ft_strncpy(dest_mine, src, size);
+		ret_mine = ft_strlcpy(dest_mine, src, size);
 		if (src)
-			ret_system = strncpy(dest_system, src, size);
+			ret_system = strlcpy(dest_system, src, size);
 	}
-	virtual ~FtStrncpyParamTest () {}
+	virtual ~FtStrlcpyParamTest () {
+	}
 
 private:
 	const char	*src = std::get<0>(GetParam());
 	size_t		size = std::get<1>(GetParam());
 };
 
-TEST_P(FtStrncpyParamTest, all) {
+TEST_P(FtStrlcpyParamTest, all) {
 	// EXPECT_STREQ(dest_mine, dest_system);
 	EXPECT_FALSE(memcmp(dest_mine, dest_system, 20)); // watch after first '\0'
-	EXPECT_EQ(dest_mine, ret_mine);
-	EXPECT_EQ(dest_system, ret_system);
+	EXPECT_EQ(ret_mine, ret_system);
 }
 
 #define P std::make_tuple<const char *, size_t>
 
-INSTANTIATE_TEST_SUITE_P(FtStrncpyTestParameters, FtStrncpyParamTest,
+INSTANTIATE_TEST_SUITE_P(FtStrlcpyTestParameters, FtStrlcpyParamTest,
 		::testing::Values(
 	P("hello", 0),
 	P("hello", 1),
@@ -70,27 +67,30 @@ INSTANTIATE_TEST_SUITE_P(FtStrncpyTestParameters, FtStrncpyParamTest,
 	P("hello", 6),
 	P("hello", 7),
 	P("hello", 12),
+	P("hello", -1),
 	P("h", 0),
 	P("h", 1),
 	P("h", 4),
 	P("h", 12),
+	P("h", -1),
 	P("", 0),
 	P("", 1),
 	P("", 4),
-	P("", 12)
+	P("", 12),
+	P("", -1)
 	));
 
 /*
 ** =============================== NULL Cases =============================== **
 */
 
-TEST(FtStrncpyTest, NullCases) {
+TEST(FtStrlcpyTest, NullCases) {
 	char	dest[10];
 	char	c;
 	*dest = 'X';
 	c = *dest;
-	EXPECT_EQ(ft_strncpy(dest, NULL, 42), (char *)NULL);
+	EXPECT_EQ(ft_strlcpy(dest, NULL, 42), 0);
 	EXPECT_EQ(*dest, c);
-	EXPECT_EQ(ft_strncpy(NULL, "hello", 42), (char *)NULL);
-	EXPECT_EQ(ft_strncpy(NULL, NULL, 42), (char *)NULL);
+	EXPECT_EQ(ft_strlcpy(NULL, "hello", 42), 0);
+	EXPECT_EQ(ft_strlcpy(NULL, NULL, 42), 0);
 }
